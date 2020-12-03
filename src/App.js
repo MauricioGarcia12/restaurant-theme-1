e3ef3043e9f45b4bf53dffa13dcf1fdf890320c6
maumainsaw <maumainsaw@gmail.com>
2020-12-02 22:02:09 -0600

    Creando productos

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom'
 import {GlobalStyle} from './GlobalStyle'
 import { Hero } from './components/Hero/Hero';
+import Products from './components/Products/Products';
+import {productData} from './components/Products/Data'
 function App() {
   return (
     <Router>
       <GlobalStyle/>
         <Hero/>
+        <Products heading='Choose your favorite' data={productData}/>
     </Router>
   );
 }
---
+++ src/components/Products/Data.jsx
@@ -0,0 +1,32 @@
+import product1 from '../../images/product1.jpg'
+import product2 from '../../images/product2.jpg'
+import product3 from '../../images/product3.jpg'
+
+
+
+export const productData = [
+    {
+        img:product1,
+        alt:'Pizza',
+        name:'Supreme Pizza',
+        desc:'Marinana sauce, basil, italian sausage, tomatoes, and pesto',
+        price:'$19.99',
+        button:'Add to Cart'
+    },
+    {
+        img:product2,
+        alt:'Pizza',
+        name:'Hawaiian Pizza',
+        desc:'Marinana sauce, basil, italian sausage, tomatoes, and pesto',
+        price:'$16.99',
+        button:'Add to Cart'
+    },
+    {
+        img:product3,
+        alt:'Pizza',
+        name:'Veggie Pizza',
+        desc:'Marinana sauce, basil, italian sausage, tomatoes, and pesto',
+        price:'$14.99',
+        button:'Add to Cart'
+    }
+]
